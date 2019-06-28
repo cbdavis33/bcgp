97dffb063fae913eadd3287ff5eb4205790f6361
Casey Davis <cbdavis33@gmail.com>
2019-06-27 19:28:09 -0700

    not sure

--- vignettes/simulate.R
+++ vignettes/simulate.R
@@ -37,14 +37,22 @@ z <- plot(changeParams, process = "y")
 
 ## ----section4------------------------------------------------------------
 
-params <- createParameterList(composite = TRUE, stationary = FALSE,
+decomp <- simulate_from_model(composite = TRUE, stationary = FALSE, 
+                              noise = FALSE, d = 1, decomposition = TRUE,
+                              seed = 701691)
+z <- plot(decomp, process = "y", decomposition = TRUE)
+
+
+## ----section5------------------------------------------------------------
+
+params <- createParameterList(composite = TRUE, stationary = TRUE,
                               noise = FALSE, d = 1)
 
-randomX <- simulate_from_model(composite = TRUE, stationary = FALSE, 
-                               randomX1D = TRUE, seed = 4384836)
+randomX <- simulate_from_model(composite = TRUE, stationary = TRUE,
+                               randomX1D = TRUE, parameters = params)
 
-seqX <- simulate_from_model(composite = TRUE, stationary = FALSE, 
-                            randomX1D = FALSE, seed = 4384836)
+seqX <- simulate_from_model(composite = TRUE, stationary = TRUE,
+                            randomX1D = FALSE, parameters = params)
 
 p1 <- plot(randomX, process = "y", print = FALSE)
 p2 <- plot(seqX, process = "y", print = FALSE)
@@ -52,23 +60,29 @@ p2 <- plot(seqX, process = "y", print = FALSE)
 gridExtra::grid.arrange(p1, p2, ncol = 2)
 
 
-## ----section5------------------------------------------------------------
+## ----section6------------------------------------------------------------
 
 params <- createParameterList(composite = TRUE, stationary = FALSE,
                               noise = FALSE, d = 2)
 
 randomXTest <- simulate_from_model(composite = TRUE, stationary = FALSE, d = 2,
-                                  parameters = params, gridTest = FALSE, 
-                                  seed = 4384836)
+                                  parameters = params, gridTest = FALSE)
 
 gridXTest <- simulate_from_model(composite = TRUE, stationary = FALSE, d = 2,
                                  parameters = params, 
-                                 gridTest = TRUE, gridTestSize = 10,
-                                 seed = 4384836)
+                                 gridTest = TRUE, gridTestSize = 10)
 
 p3 <- plot(randomXTest, process = "y", print = FALSE)
 p4 <- plot(gridXTest, process = "y", print = FALSE)
 
 gridExtra::grid.arrange(p3, p4, ncol = 2)
+
+
+## ----raster--------------------------------------------------------------
+
+p5 <- plot(gridXTest, process = "y", print = FALSE, raster = FALSE)
+p6 <- plot(gridXTest, process = "y", print = FALSE, raster = TRUE)
+
+gridExtra::grid.arrange(p5, p6, ncol = 2)
 
 
