11de967bfa3d2f13628b543e9788be1f2bff313a
Casey Davis <cdavis09@amgen.com>
2019-09-17 04:20:45 -0700

    Updated 9/17

--- scratch/scratch.R
+++ scratch/scratch.R
@@ -1,13 +1,21 @@
 rm(list = ls())
 cat("\014")
 
-simData <- bcgpsims(composite = FALSE, stationary = TRUE, noise = FALSE, d = 1)
+pars <- create_parameter_list(composite = TRUE, stationary = FALSE, noise = FALSE, d = 1)
+
+seed <- sample(1e5, 1)
+seed <- 96762
+print(seed) # 5088
+simData <- bcgpsims(composite = TRUE, stationary = FALSE, noise = FALSE, d = 1,
+                    parameters = pars, seed = seed, decomposition = TRUE)
+plot(simData, decomposition = TRUE)
 
 
-tmp <- stanNonCompS(simData@training$x, simData@training$y,
-                    cores = 4,
-                    control = list(adapt_delta = 0.99,
-                                   max_treedepth = 15))
+modelCompNSScaled <- bcgpmodel(x = simData@training$x, y = simData@training$y,
+                               composite = TRUE, stationary = FALSE, noise = FALSE)
+fitCompNSScaled <- bcgp_sampling(modelCompNSScaled, scaled = TRUE, cores = 4, nmcmc = 500, burnin = 200)
 
-print(tmp, digits = 4)
-shinystan::launch_shinystan(tmp)
+fitCompNSScaled
+print(fitCompNSScaled, digits_summary = 3)
+bcgp:::print.bcgpfit(fitCompNSScaled, digits_summary = 3, pars = c("beta0", "w", "rhoG"))
+summary(fitCompNSScaled)
